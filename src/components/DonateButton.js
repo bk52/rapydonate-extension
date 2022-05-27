654b992ba8f2f65d223f6d2c608dfb46f2687f02
bk52 <baris_karamustafa52@hotmail.com>
2022-05-27 21:29:45 +0300

    Project Info Page

--- src/components/DonateButton.js
+++ src/components/DonateButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DonateButton = ({ id, icon, tooltip, onDonateClick }) => {
-    return <div title={tooltip} onClick={(id) => onDonateClick(id)} className="w-16 h-16 rounded-full bg-white shadow-md cursor-pointer flex justify-center items-center text-2xl hover:shadow-none">
+    return <div title={tooltip} onClick={(e) => onDonateClick(id)} className="w-16 h-16 rounded-full bg-white shadow-md cursor-pointer flex justify-center items-center text-2xl hover:shadow-none">
         {icon}
     </div>
 }
--- src/pages/Info.js
+++ src/pages/Info.js
@@ -12,8 +12,10 @@ const Info = () => {
     const dispatch = useDispatch();
     const project = useSelector(selectProject);
     const [loading, setLoading] = useState(true);
-    const onDonateClick = () => {
-
+    const [donate, setDonate] = useState(null);
+    const onDonateClick = (id) => {
+        const selected = project.donationTypes.filter(item => item._id === id)[0];
+        setDonate(selected);
     }
     useEffect(() => {
         const GetData = async () => {
@@ -43,10 +45,27 @@ const Info = () => {
     return loading ? <LoadingPage /> : <div className="flex flex-col w-full h-full">
         {project.imageURL != '' && <div className="w-full h-52"><img src={project.imageURL} className="h-full w-full object-cover" /></div>}
         <div className="w-full text-xl text-center mt-2 text-gray-800 font-bold">{project.title}</div>
-        <div className="w-full text-lg mt-1 text-gray-400 px-2">{project.description}</div>
-        <div className="flex flex-row w-full mt-4 justify-evenly">
-            {project.donationTypes.map(item => <DonateButton id={item._id} icon={item.icon} tooltip={item.title} onDonateClick={onDonateClick} />)}
-        </div>
+        {
+            donate ? <>
+                <div class="flex flex-col items-center px-2">
+                    <div className="w-full text-lg mt-6 text-center text-gray-700">{donate.title}</div>
+                    <div className="w-full text-xl mt-4 text-center text-black font-bold">{donate.price} $</div>
+                    {donate.donateType === 'message' && <input className="p-2 w-full mt-4 text-gray-400 border-none focus:ring-0" placeholder="Max. 180 character" maxlength="180" />}
+                    {/* <select name="selectPaymentMethod" id="selectPaymentMethod" class="p-2 w-48 mt-2 text-gray-400 border-none focus:ring-0">
+                            <option value="-">Select payment method</option>
+                        </select> */}
+                    <div className="flex">
+                        <div className="w-24 p-2 text-center cursor-pointer bg-white shadow-md rounded-sm mt-6 text-gray-400 hover:text-gray-600">Continue</div>
+                        <div onClick={(e) => { setDonate(null) }} className="w-24 p-2 text-center cursor-pointer bg-white shadow-md rounded-sm mt-6 text-red-400 hover:text-red-600">Back</div>
+                    </div>
+                </div>
+            </> : <>
+                <div className="w-full text-lg mt-1 text-gray-400 px-2">{project.description}</div>
+                <div className="flex flex-row w-full mt-4 justify-evenly">
+                    {project.donationTypes.map(item => <DonateButton id={item._id} icon={item.icon} tooltip={item.title} onDonateClick={onDonateClick} />)}
+                </div>
+            </>
+        }
     </div>
 }
 
